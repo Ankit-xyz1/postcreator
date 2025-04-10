54c60b4c5e0e046c8aece32d10b53cf465f30a93
Ankit-xyz1 <shahankit023@gmail.com>
2025-04-10 16:24:14 +0530

    all feature added

--- app/api/genratePost/route.ts
+++ app/api/genratePost/route.ts
@@ -3,8 +3,9 @@ import { GoogleGenAI } from "@google/genai";
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 export async function POST(request: Request) {
-  const res = await request.json();
-  const response = await ai.models.generateContent({
+  const { password, context } = await request.json();
+  if (password != process.env.PASSWORD) return Response.json({ sucess: false, message: "password is wrong" })
+  const x_Response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
     config: {
       maxOutputTokens: 1000,
@@ -12,8 +13,21 @@ export async function POST(request: Request) {
       systemInstruction:
         "You are a professional social media content creator who writes engaging posts specifically for platforms like LinkedIn and Twitter. I will provide you with context or a topic, and your job is to generate one compelling, creative, and well-structured post tailored to the specified platform. The post should reflect the tone, style, and audience expectations of that platform—professional and insightful for LinkedIn, concise yet engaging for Twitter. Ensure the response is clear, logical, and impactful. Write **only one** complete post per request, and make sure it is at least **100 words** long. Avoid generic responses—make each post feel original and platform-appropriate.",
     },
-    contents: "write a general post for my twitter that i created this project https://github.com/Ankit-xyz1/githubanalyzer ",
+    contents: `here is my context ${context} , create an engaging post of 250-letters max regarding to the context for my twitter only give me post no extra words `,
   });
-  const data = response.text;
-  return Response.json({ data });
+
+  const LinkedIn_Rresponse = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    config: {
+      maxOutputTokens: 1000,
+      temperature: 1,
+      systemInstruction:
+        "You are a professional social media content creator who writes engaging posts specifically for platforms like LinkedIn and Twitter. I will provide you with context or a topic, and your job is to generate one compelling, creative, and well-structured post tailored to the specified platform. The post should reflect the tone, style, and audience expectations of that platform—professional and insightful for LinkedIn, concise yet engaging for Twitter. Ensure the response is clear, logical, and impactful. Write **only one** complete post per request, and make sure it is at least **100 words** long. Avoid generic responses—make each post feel original and platform-appropriate.",
+    },
+    contents: `here is my context ${context} , create an engaging post regarding to the context for my LinkedIn in linkedInstyle only give me post no extra words `,
+  });
+
+  const Xpost =  x_Response.text;
+  const LinkedIn_post = LinkedIn_Rresponse.text;
+  return Response.json({ sucess:true , message:"post created sucessfully" , x: Xpost ,linkedIn : LinkedIn_post });
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 import { BackgroundLines } from "@/components/ui/background-lines";
 import { useState } from "react";
 import toast, { Toaster } from 'react-hot-toast';
+import ReactMarkdown from 'react-markdown';
+import { CheckCheck, Copy, Loader, SquareArrowOutUpRight } from 'lucide-react'
 
 
 export default function Home() {
@@ -10,9 +12,12 @@ export default function Home() {
   const [pass, setpass] = useState<string>("")
 
   //state for the app
+  const [copy, setcopy] = useState(false)
   const [loading, setloading] = useState<boolean>(false)
   const [postData, setpostData] = useState<any[]>([])
 
+  const [LinkedInpost, setLinkedInpost] = useState<string>("")
+  const [xPost, setxPost] = useState<string>("")
 
   //functions for hadnling input 
   const handleContext = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -32,10 +37,10 @@ export default function Home() {
       //sets loading state to true
       setloading(true)
       //fetching data from api
-      const response = await fetch('/api/genratePost',{
-        method:"POST",
-        body:JSON.stringify({
-          password:pass,
+      const response = await fetch('/api/genratePost', {
+        method: "POST",
+        body: JSON.stringify({
+          password: pass,
           context
         })
       })
@@ -43,10 +48,23 @@ export default function Home() {
       const data = await response.json()
       console.log(data)
 
-      //an sucess toast
-      toast.error('genrated sucessfully');
-      //sets loading state to false
-      setloading(false)
+      if (!data.sucess) {
+        setContext("");
+        setpass("");
+        return toast.error(data.message);
+      }
+
+
+      if (data.sucess) {
+        // updating post state
+        setxPost(data.x);
+        setLinkedInpost(data.linkedIn);
+        //an sucess toast
+        toast.success('genrated sucessfully');
+        //sets loading state to false
+        setContext("");
+        setloading(false)
+      }
 
 
     } catch (error) {
@@ -55,17 +73,30 @@ export default function Home() {
       setloading(false)
       toast.error('some error occured');
     }
-    
+
   }
 
+  //copying an post 
+  const copyText = async (data: string) => {
+    setcopy(true)
+    await navigator.clipboard.writeText(data)
+    toast.success("copied sucessfully")
+    setTimeout(() => {
+      setcopy(false)
+    }, 1000);
+  }
+
+  const post = () => {
+    window.open(`https://twitter.com/intent/tweet?text=${encodeURIComponent(xPost)}`);
+  }
   return (
     <BackgroundLines className="flex  bg-zinc-950 items-center justify-center h-screen w-full flex-col px-4 p-10">
-      <div className="h-screen md:w-[40%] w-full bg-zinc-900 p-5 rounded-3xl relative z-[9999999999999999] jet">
+      <div className="h-screen md:w-[60%] w-full bg-zinc-900 p-5 rounded-3xl relative z-[9999999999999999] jet">
         <Toaster />
-        <div className="context  flex flex-col gap-4">
+        <div className="context h-fit flex flex-col gap-4">
           <textarea
             rows={5}
-            className="text-white bg-zinc-800 p-4 rounded-xl focus:outline-none resize-none md:text-lg"
+            className="text-white bg-zinc-800 p-4 rounded-xl focus:outline-none resize-none md:text-sm"
             placeholder="Give me some context"
             value={context}
             onChange={handleContext}
@@ -90,6 +121,50 @@ export default function Home() {
               </div>
             </button>
           </div>
+        </div>
+        <div className=" h-[50vh] w-full flex gap-2 flex-wrap justify-center overflow-auto p-5  text-[9px] md:text-xs">
+          {loading ?<div className="h-full w-full flex items-center justify-center"> <Loader className="animate-spin text-white" size={25} /> </div>: <>
+
+            {xPost &&
+              <div className="card min-h-fit w-[500px] text-white bg-zinc-800 p-3 relative rounded text-[9px] md:text-xs" >
+                <h1 className="text-xl mb-4 text-purple-400">X Post</h1>
+                <ReactMarkdown>
+                  {xPost}
+                </ReactMarkdown>
+                <div className="copy post absolute top-2 right-2 w-fit flex gap-1">
+                  <button className="relative group cursor-pointer bg-zinc-950 hover:bg-zinc-700 p-2 rounded h-fit transition-all ease-in-out duration-200" onClick={post}>
+                    <SquareArrowOutUpRight size={12} />
+                    <span className="absolute left-1/2 -translate-x-1/2 mt-2 w-max opacity-0 group-hover:opacity-100 transition bg-black text-white text-xs px-2 py-1 rounded z-10">
+                      Post on X
+                    </span>
+                  </button>
+                  <button className="relative group cursor-pointer bg-zinc-950 hover:bg-zinc-700 p-2 rounded h-fit transition-all ease-in-out duration-200" onClick={() => copyText(LinkedInpost)}>
+                    {copy ? <CheckCheck size={12} /> : <Copy size={12} />}
+                    <span className="absolute left-1/2 -translate-x-1/2 mt-2 w-max opacity-0 group-hover:opacity-100 transition bg-black text-white text-xs px-2 py-1 rounded z-10">
+                      Copy
+                    </span>
+                  </button>
+                </div>
+              </div>
+            }
+            {LinkedInpost &&
+              <div className="card min-h-fit w-[500px] text-white bg-zinc-800 p-3 relative rounded" >
+                <h1 className="text-xl mb-4 text-purple-400">LinkedIn Post</h1>
+                <ReactMarkdown>
+                  {LinkedInpost}
+                </ReactMarkdown>
+                <div className="copy post absolute top-2 right-2 w-fit flex gap-1">
+                  <button className="relative group cursor-pointer bg-zinc-950 hover:bg-zinc-700 p-2 rounded h-fit transition-all ease-in-out duration-200" onClick={() => copyText(LinkedInpost)}>
+                    {copy ? <CheckCheck size={12} /> : <Copy size={12} />}
+                    <span className="absolute left-1/2 -translate-x-1/2 mt-2 w-max opacity-0 group-hover:opacity-100 transition bg-black text-white text-xs px-2 py-1 rounded z-10">
+                      Copy
+                    </span>
+                  </button>
+                </div>
+              </div>
+            }
+          </>
+          }
         </div>
       </div>
     </BackgroundLines>
